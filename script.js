850b77f2956a159dae1174423fef7c7a66b7a2af
elliottfeltham <elliottfeltham98@gmail.com>
2025-01-15 22:23:22 +0000

    Completed first rock, paper, scissors project

--- script.js
+++ script.js
@@ -11,29 +11,70 @@ function getComputerChoice() {
     }
 };
 
-console.log(getComputerChoice())
-
 // Get users choice with prompt
 
 function getHumanChoice() {
-    let answer = prompt("Enter your choice: ");
-    let userChoice = answer.charAt(0).toUpperCase() + answer.slice(1).toLowerCase();
-    // Add input validation and error handling later
+    let answer = prompt("Enter your choice; Rock, Paper or Scissors: ");
 
-    return userChoice
+    // Check that answer is not null
+    if (!answer) {
+        return getHumanChoice();
+    }
+
+    // Check answer is stylized correctly
+    let humanChoice = answer.charAt(0).toUpperCase() + answer.slice(1).toLowerCase();
+
+    // Check that answer is valid
+    if (humanChoice !== "Rock" && humanChoice !== "Paper" && humanChoice !== "Scissors") {
+        return getHumanChoice();
+    } 
+    return humanChoice;
 }
-
-let test = getHumanChoice();
-
-console.log(test);
-// Compute winner with conditionals
 
 // Initialize each players score variables
 
-// Write logic to play a single round
+let humanScore = 0;
+let computerScore = 0;
+
+// Write logic to play a single round and compute winner with conditionals
+
+function playRound(computerChoice, humanChoice) {
+    if (humanChoice === "Rock" && computerChoice === "Scissors") {
+        console.log("You win! Rock beats Scissors!");
+        humanScore ++;
+    } else if (humanChoice === "Paper" && computerChoice === "Rock") {
+        console.log("You win! Paper beats Rock!");
+        humanScore ++;
+    } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
+        console.log("You win! Scissors beats Paper!");
+        humanScore ++;
+    } else if (humanChoice === computerChoice) {
+        console.log(`Its a tie! You both chose ${humanChoice}`);
+    } else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    }
+}
 
 // Write logic to play 5 rounds and track the score
 
+function playGame() {
+
+    for (i = 0; i < 5; i++) {
+        const computerChoice = getComputerChoice();
+        const humanChoice = getHumanChoice();
+        playRound(computerChoice, humanChoice);
+    }
+
+    if (humanScore > computerScore) {
+        console.log(`You win! You: ${humanScore} - Computer: ${computerScore}`);
+    } else if (humanScore < computerScore) {
+        console.log(`You lose! You: ${humanScore} - Computer: ${computerScore}`);
+    } else {
+        console.log(`It's a tie! You: ${humanScore} - Computer: ${computerScore}`);
+    }
+}
 
 
+playGame();
 
