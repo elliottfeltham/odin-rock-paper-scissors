7b3b7e1f009d6b5ebe322303a03a74999d5361b5
elliottfeltham <elliottfeltham98@gmail.com>
2025-01-15 18:50:48 +0000

    Add psuedocode for project

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello, World!");
+// Get Computers choice with random number generator
+
+// Get users choice with prompt
+
+// Compute winner with conditionals
+
+// Initialize each players score variables
+
+// Write logic to play a single round
+
+// Write logic to play 5 rounds and track the score
+
+
+
+
