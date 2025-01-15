25a32320b42c7c8b5bcebcee27edd27fbc8a1bc4
elliottfeltham <elliottfeltham98@gmail.com>
2025-01-15 19:04:21 +0000

    Add computer choice function

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 // Get Computers choice with random number generator
 
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3);
+    if (computerChoice === 0) {
+        return "Rock";
+    } else if (computerChoice === 1) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+};
+
+console.log(getComputerChoice())
+
 // Get users choice with prompt
 
 // Compute winner with conditionals
