0ef1860a60296c12a56bac86000194fd633da960
elliottfeltham <elliottfeltham98@gmail.com>
2025-01-15 19:24:15 +0000

    Add getHumanChoice function

--- script.js
+++ script.js
@@ -15,6 +15,15 @@ console.log(getComputerChoice())
 
 // Get users choice with prompt
 
+function getHumanChoice() {
+    let answer = prompt("Enter your choice: ");
+    let userChoice = answer  // Add input validation and error handling later
+    return userChoice
+}
+
+let test = getHumanChoice();
+
+console.log(test);
 // Compute winner with conditionals
 
 // Initialize each players score variables
