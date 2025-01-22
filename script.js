bbb9d9cc862a630e62c4e00c7556f45621169e1b
elliottfeltham <elliottfeltham98@gmail.com>
2025-01-22 09:02:36 +0000

    Add results background colors to buttons

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const buttons = document.querySelectorAll("button");
 const rockButton = document.querySelector("#rock");
 const paperButton = document.querySelector("#paper");
 const scissorsButton = document.querySelector("#scissors");
@@ -23,29 +24,38 @@ function getComputerChoice() {
 let humanScore = 0;
 let computerScore = 0;
 
+
 // Write logic to play a single round and compute winner with conditionals
 
 function playRound(humanChoice, computerChoice) {
+
+    let roundWinner = null;
+
     if (humanChoice === "Rock" && computerChoice === "Scissors") {
         roundResult.textContent = "You win! Rock beats Scissors!";
         humanScore ++;
+        roundWinner = "human";
     } else if (humanChoice === "Paper" && computerChoice === "Rock") {
         roundResult.textContent = "You win! Paper beats Rock!";
         humanScore ++;
+        roundWinner = "human";
     } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
         roundResult.textContent = "You win! Scissors beats Paper!";
         humanScore ++;
+        roundWinner = "human";
     } else if (humanChoice === computerChoice) {
         roundResult.textContent = `Its a tie! You both chose ${humanChoice}`;
     } else {
         roundResult.textContent = `You lose! ${computerChoice} beats ${humanChoice}`;
         computerScore++;
+        roundWinner = "computer";
     }
 
     humanScoreDisplay.textContent = humanScore;
     computerScoreDisplay.textContent = computerScore;
 
     checkWinner();
+    return roundWinner;
 }
 
 // Write logic to play 5 rounds and track the score
@@ -66,16 +76,28 @@ function resetGame() {
     computerScoreDisplay.textContent = computerScore;
 }
 
+function resetButton() {
+    buttons.forEach((button) => {
+        button.style.backgroundColor = "white";
+    }); 
+}
+
 rockButton.addEventListener("click", () => {
-    playRound("Rock", getComputerChoice());
+    const winner = playRound("Rock", getComputerChoice());
+    rockButton.style.backgroundColor = winner === "human" ? "rgba(0, 255, 50, 0.5)" : winner === "computer" ? "rgba(255, 0, 0, 0.5)" : "white";
+    setTimeout(resetButton, 2500);
 });
-    
+
 paperButton.addEventListener("click", () => {
-    playRound("Paper", getComputerChoice());
+    const winner = playRound("Paper", getComputerChoice());
+    paperButton.style.backgroundColor = winner === "human" ? "rgba(0, 255, 0, 0.5)" : winner === "computer" ? "rgba(255, 0, 0, 0.5)" : "white";
+    setTimeout(resetButton, 2500);
 });
 
 scissorsButton.addEventListener("click", () => {
-    playRound("Scissors", getComputerChoice());
+    const winner = playRound("Scissors", getComputerChoice());
+    scissorsButton.style.backgroundColor = winner === "human" ? "rgba(0, 255, 0, 0.5)" : winner === "computer" ? "rgba(255, 0, 0, 0.5)" : "white";
+    setTimeout(resetButton, 2500);
 });
 
 
